a60a328c1b040c2a6cb183f1b0cff5bb0e97f419
zhengqingquan <55186872+zhengqingquan@users.noreply.github.com>
2023-03-14 23:28:46 +0800

    修改一些细节

--- src/pages/EditUserPage.vue
+++ src/pages/EditUserPage.vue
@@ -1,21 +1,11 @@
 <template>
   <van-form @submit="onSubmit">
-    <van-cell-group inset>
       <van-field
           :v-model="editUser.currentValue"
           :name="editUser.editKey"
           :label="editUser.editName"
-          :rules="[{ required: true, message: '请填写用户名' }]"
+          :placeholder="`请输入${editUser.editName}`"
       />
-      <van-field
-          v-model="password"
-          type="password"
-          name="密码"
-          label="密码"
-          placeholder="密码"
-          :rules="[{ required: true, message: '请填写密码' }]"
-      />
-    </van-cell-group>
     <div style="margin: 16px;">
       <van-button round block type="primary" native-type="submit">
         提交
